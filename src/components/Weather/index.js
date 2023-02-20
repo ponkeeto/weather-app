3aee6220db9ace40a757dcab3de6699aa76d651c
ponkeeto <ponchsalvador@gmail.com>
2023-02-20 23:48:53 +0800

    show github user data in default page

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,8 +1,8 @@
 import { Grid, Typography } from "@mui/material";
-import Wrapper from "../AuthWrapper";
-import LoginButton from "../LoginButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Weather = () => {
+  const { user } = useAuth0();
   return (
     <Grid
       container
@@ -20,18 +20,8 @@ const Weather = () => {
         xs={6}
         style={{ padding: "20px", minHeight: "50%", marginTop: "10%" }}
       >
-        <Wrapper>
-          <Grid item xs={12} style={{ marginBottom: "15px" }}>
-            <Typography gutterBottom textAlign="left" variant="h6">
-              Welcome to the weather forecast web application. Please login your
-              Github user to use the application and view the weather in your
-              city.
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <LoginButton />
-          </Grid>
-        </Wrapper>
+        <Typography>{user.name}</Typography>
+        <Typography>https://github.com/{user.nickname}</Typography>
       </Grid>
     </Grid>
   );
